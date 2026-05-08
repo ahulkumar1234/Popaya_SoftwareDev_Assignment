4d3df1045e669a23e9edf92475023455523f7279
Rahul Kumar <rahulkumar8340527941@gmail.com>
2026-05-08 18:36:15 +0530

    backend deployed

--- frontend/notes-ui/src/services/api.js
+++ frontend/notes-ui/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:9000"
+  baseURL: "https://popaya-softwaredev-assignment-1.onrender.com"
 });
 
 export default API;
